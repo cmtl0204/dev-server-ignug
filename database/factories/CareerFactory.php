1caa9087488cecbbc63f85451a9f2a9522a25867
cmtl0204 <cesar.tamayo0204@gmail.com>
2020-08-24 19:24:54 -0500

    cambios

--- database/factories/CareerFactory.php
+++ database/factories/CareerFactory.php
@@ -2,7 +2,7 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Career;
+use App\Models\Ignug\Career;
 use Faker\Generator as Faker;
 
 $factory->define(Career::class, function (Faker $faker) {
